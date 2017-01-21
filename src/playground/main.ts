e0ba3c94ec8814d9556c67f2105f6bdb63a75c5a
David Schnell-Davis <dschnelldavis@gmail.com>
2017-01-21 11:01:53 -0800

    Adds commented enableProdMode to main.ts to make is easier to turn on production mode for playground.

--- src/playground/main.ts
+++ src/playground/main.ts
@@ -1,6 +1,9 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { PlaygroundModule } from './playground.module';
+
+// enableProdMode();
 
 export function main() {
   platformBrowserDynamic().bootstrapModule(PlaygroundModule);
