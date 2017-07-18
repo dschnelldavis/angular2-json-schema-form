d50681d38e48afc6e9ea29e8c1bf752ef2483126
George Oakling <george@oakling.name>
2017-07-18 16:10:10 +0200

    Update material-design-framework.module.ts

--- src/lib/src/framework-library/material-design-framework/material-design-framework.module.ts
+++ src/lib/src/framework-library/material-design-framework/material-design-framework.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }                         from '@angular/core';
 import { CommonModule }                     from '@angular/common';
-import { BrowserAnimationsModule }          from '@angular/platform-browser/animations';
+// import { BrowserAnimationsModule }          from '@angular/platform-browser/animations';
 import { FlexLayoutModule }                 from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
