99b50198579b5ce19a8388b00da41d10eb27af2a
David Schnell-Davis <dschnelldavis@gmail.com>
2017-11-14 18:59:58 -0800

    Import hammerjs in main.ts instead of .angular-cli.json.

--- src/demo/main.ts
+++ src/demo/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { DemoModule } from './app/demo.module';
 import { environment } from './environments/environment';
 
+import 'hammerjs';
+
 if (environment.production) { enableProdMode(); }
 
 platformBrowserDynamic().bootstrapModule(DemoModule);
